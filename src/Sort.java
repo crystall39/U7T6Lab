94147000dab37d043bb928de434e315d0585af4b
BT_1E10_27 <wrwgwgh>
2022-02-17 13:51:33 -0500

    Finished lab

--- src/Sort.java
+++ src/Sort.java
@@ -3,13 +3,15 @@ import java.util.ArrayList;
 public class Sort
 {
   // Part A.  Which Sort is Which?
-  public static void firstSortingAlgorithm(int[] elements)
+  public static void selectionSort(int[] elements)
   {
+    int runTime = 0;
     for (int j = 0; j < elements.length - 1; j++)
     {
       int minIndex = j;
       for (int k = j + 1; k < elements.length; k++)
-      {      
+      {
+        runTime++;
         if (elements[k] < elements[minIndex])
         {
           minIndex = k;
@@ -19,33 +21,66 @@ public class Sort
       elements[j] = elements[minIndex];
       elements[minIndex] = temp;
     }
+    System.out.println("Run Time: " + runTime);
   }
 
   // Part A.  Which Sort is Which?
-  public static void secondSortingAlgorithm(int[] elements)
-  { 
+  public static void insertionSort(int[] elements)
+  {
+    int runTime = 0;
     for (int j = 1; j < elements.length; j++)
     {
       int temp = elements[j];
       int possibleIndex = j;
       while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
-      {     
+      {
+        runTime++;
         elements[possibleIndex] = elements[possibleIndex - 1];
         possibleIndex--;
       }
       elements[possibleIndex] = temp;
     }
+    System.out.println("Run Time: " + runTime);
   }
   
   // Part C.  Sorting a 1000-word list!
   public static void selectionSortWordList(ArrayList<String> words)
   {
-    /* TO BE IMPLEMENTED IN PART C */
+    int runTime = 0;
+    for (int j = 0; j < words.size() - 1; j++)
+    {
+      int minIndex = j;
+      for (int k = j + 1; k < words.size(); k++)
+      {
+        runTime++;
+        if (words.get(k).compareTo(words.get(minIndex)) < 0)
+        {
+          minIndex = k;
+        }
+      }
+      String temp = words.get(j);
+      words.set(j, words.get(minIndex));
+      words.set(minIndex, temp);
+    }
+    System.out.println("Run Time: " + runTime);
   }
 
   // Part C.  Sorting a 1000-word list!
   public static void insertionSortWordList(ArrayList<String> words)
-  {    
-    /* TO BE IMPLEMENTED IN PART C */
+  {
+    int runTime = 0;
+    for (int j = 1; j < words.size(); j++)
+    {
+      String temp = words.get(j);
+      int possibleIndex = j;
+      while (possibleIndex > 0 && temp.compareTo(words.get(possibleIndex - 1)) < 0)
+      {
+        runTime++;
+        words.set(possibleIndex, words.get(possibleIndex - 1));
+        possibleIndex--;
+      }
+      words.set(possibleIndex, temp);
+    }
+    System.out.println("Run Time: " + runTime);
   }
 }
--- src/SortTester.java
+++ src/SortTester.java
@@ -13,5 +13,89 @@ public class SortTester
     int[] arr2 = {3, 86, -20, 14, 40, 19, -4, 100, 89, 13, 54, 12, 77, -5, 0, 6};    
     Sort.insertionSort(arr2);
     System.out.println(Arrays.toString(arr2));
+
+    /*
+    // --- SELECTION SORT ---
+    int[] arr3 = {77, -5, -4, 100, 12, 6, 89, 13, 0, 54, 3, 86, -20, 14, 40, 19};
+    Sort.selectionSort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
+    // --- INSERTION SORT ---
+    int[] arr4 = {77, -5, -4, 100, 12, 6, 89, 13, 0, 54, 3, 86, -20, 14, 40, 19};
+    Sort.insertionSort(arr4);
+    System.out.println(Arrays.toString(arr4));
+    */
+
+    /*
+    // --- SELECTION SORT ---
+    int[] arr3 = {100, 89, 86, 77, 54, 40, 19, 14, 13, 12, 6, 3, 0, -4, -5, -20};
+    Sort.selectionSort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
+    // --- INSERTION SORT ---
+    int[] arr4 = {100, 89, 86, 77, 54, 40, 19, 14, 13, 12, 6, 3, 0, -4, -5, -20};
+    Sort.insertionSort(arr4);
+    System.out.println(Arrays.toString(arr4));
+    */
+
+    /*
+    // --- SELECTION SORT ---
+    int[] arr3 = {-5, -20, 0, -4, 3, 13, 6, 12, 14, 40, 19, 54, 77, 89, 100, 86};
+    Sort.selectionSort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
+    // --- INSERTION SORT ---
+    int[] arr4 = {-5, -20, 0, -4, 3, 13, 6, 12, 14, 40, 19, 54, 77, 89, 100, 86};
+    Sort.insertionSort(arr4);
+    System.out.println(Arrays.toString(arr4));
+    */
+
+    /*
+    // --- SELECTION SORT ---
+    int[] arr3 = {-20, -5, -4, 0, 3, 6, 12, 13, 14, 19, 40, 54, 77, 86, 89, 100};
+    Sort.selectionSort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
+    // --- INSERTION SORT ---
+    int[] arr4 = {-20, -5, -4, 0, 3, 6, 12, 13, 14, 19, 40, 54, 77, 86, 89, 100};
+    Sort.insertionSort(arr4);
+    System.out.println(Arrays.toString(arr4));
+    */
+
+    /*
+    // --- SELECTION SORT ---
+    int[] arr3 = {2, 5, 1, 4, 7, 9, 21, 6, 64, 12, 0, -2, -4, -7, -34, 18};
+    Sort.selectionSort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
+    // --- INSERTION SORT ---
+    int[] arr4 = {2, 5, 1, 4, 7, 9, 21, 6, 64, 12, 0, -2, -4, -7, -34, 18};
+    Sort.insertionSort(arr4);
+    System.out.println(Arrays.toString(arr4));
+    */
+
+    /*
+    // --- SELECTION SORT ---
+    int[] arr3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+    Sort.selectionSort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
+    // --- INSERTION SORT ---
+    int[] arr4 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+    Sort.insertionSort(arr4);
+    System.out.println(Arrays.toString(arr4));
+    */
+
+    /*
+    // --- SELECTION SORT ---
+    int[] arr3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    Sort.selectionSort(arr3);
+    System.out.println(Arrays.toString(arr3));
+
+    // --- INSERTION SORT ---
+    int[] arr4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    Sort.insertionSort(arr4);
+    System.out.println(Arrays.toString(arr4));
+    */
   }
 }
